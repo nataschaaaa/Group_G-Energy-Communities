e64d2c2b6aba17adba317d6a0359dad34aa905b6
wi24b082 <wi24b082@technikum-wien.at>
2026-04-16 15:38:06 +0200

    feat: add App.java entry point and change file names

---
+++ energy-javafx-gui/src/main/java/at/fhtw/energyjavafxgui/App.java
@@ -0,0 +1,23 @@
+package at.fhtw.energyjavafxgui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Energy Community Monitor");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
